6135b5f8534255eca4003f06e6d1d450a1f73997
Adam Labas <adam.labas@mail.utoronto.ca>
2022-02-02 19:04:24 +0000

    edits made to the R-script

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,71 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Download and clean data from opendatatoronto
+# Author: Adam Labas
+# Data: 6 Febuary 2022
+# Contact: adam.labas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Need to have downloaded the data from opendatatoronto and saved it to inputs/data
 # - Don't forget to gitignore it!
-# - Change these to yours
+
 # Any other information needed?
 
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
-library(haven)
+
 library(tidyverse)
+library(opendatatoronto)
+
+
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+#raw_data <- readr::read_csv("inputs/data/raw_data.csv"
                      )
+################## clean data here ###################################
+
 # Just keep some variables that may be of interest (change 
 # this depending on your interests)
-names(raw_data)
-
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+# names(raw_data)
+# 
+# reduced_data <- 
+#   raw_data %>% 
+#   select(first_col, 
+#          second_col)
+# rm(raw_data)
          
 
 #### What's next? ####
 
+library(opendatatoronto)
+library(dplyr)
 
+### Download Data ###
+# Data located at the following URL: 
+# https://open.toronto.ca/dataset/toronto-shelter-system-flow/
+
+# Datasets are grouped into packages that have multiple datasets that are
+# relevant to that topic. So we first look at the package using a unique
+# key that we obtain from the dataset website linked above.
+
+# get package
+package <- show_package("ac77f532-f18b-427c-905c-4ae87ce69c93")
+package
+
+# get all resources for this package
+resources <- list_package_resources("ac77f532-f18b-427c-905c-4ae87ce69c93")
+
+# We need the unique key from that list of resources.
+
+# There is only one resource in our case so get_resources() will load it.
+monthly_shelter_usage <- 
+  resources %>% 
+  get_resource()
+
+monthly_shelter_usage
+
+### Save the data in our inouts folder incase the data is removed or
+# relocated from the provided URL ###
+
+write.csv(x = monthly_shelter_usage,
+          file = "inputs/data/monthly_shelter_usage.csv")
 
          
